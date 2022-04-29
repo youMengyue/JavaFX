7b3e0bded8014e1e0211e9ec93022b86e5fcd8e8
youMengyue <1599214182@qq.com>
2022-04-29 20:16:54 +0800

    第二次增加Demo

---
+++ src/main/java/com/example/javafxhomwork/Demo.java
@@ -0,0 +1,17 @@
+package com.example.javafxhomwork;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Demo extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        primaryStage.show();
+    }
+}
